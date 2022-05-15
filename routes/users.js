9fc39c43153af9d792ee4c48dc67db0c80b892d4
Alessandro Zanola <ales.zanola@gmail.com>
2022-05-15 11:22:47 +0200

    Init registration endpoint

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', function (req, res, next) {
+  console.log(req.body);
+  res.send();
+})
+
 module.exports = router;
