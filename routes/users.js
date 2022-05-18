1105c82ad1dda65c9c587d263d17b9615d7de7b8
Giuseppe Alessio Sciumè <sciumegpp@gmail.com>
2022-05-18 11:34:45 +0200

    Add basic registration (no controls and plain passwords)

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,21 @@
 var express = require('express');
+const User = require('../models/User');
 var router = express.Router();
+
+// db creation
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@easylockertest.5tuqt.mongodb.net/?retryWrites=true&w=majority');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', function (req, res, next) {
+//Create a user
+router.post('/register', async function (req, res, next) {
   console.log(req.body);
+  const user = new User(req.body);
+  user.save();
   res.send();
 })
 
